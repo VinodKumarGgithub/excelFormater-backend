abecb5a65ee18aa7a44b7a95840b94def521ea09
Vinod Kumar G <kvinod@medimpactintl.com>
2025-05-21 10:50:21 +0530

    feat: Update IP check middleware to prioritize client IP header

--- middleware/ip.js
+++ middleware/ip.js
@@ -1,7 +1,8 @@
 const allowedIPs = process.env.ALLOWED_IPS?.split(',') || ['127.0.0.1', '::1', '192.168.1.100'];
 
 export const checkIP = (req, res, next) => {
-  const ip = req.ip || req.connection.remoteAddress;
+  // const ip = req.ip || req.connection.remoteAddress;
+  const ip = req.headers['x-client-ip'] || req.ip;
   if (allowedIPs?.includes(ip)) {
     return next();
   }
